23038fbeeb937a0092b080a8cf0ac6cbeaffe58f
Spiroti <56719843+Spiroti@users.noreply.github.com>
2021-04-08 13:52:52 +0200

    Update QRCode.tsx

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -13,7 +13,7 @@ export default function App() {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = ({ type = "" , data = ""}) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
